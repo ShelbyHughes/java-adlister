aa5bfc39c8db8a6e7ba05da7397422c7632e39b6
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-02-26 12:15:07 -0600

    try again again

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -6,18 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
-    private String name = "Dylan";
+@WebServlet(name = "Count", urlPatterns = "/count")
+public class Count extends HttpServlet {
+    private int counter = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        if (name.equals(null) || name.equals("") || name.equals(" ")) {
-            out.println("<h1>Hello, World!</h1>");
-        } else {
-            out.println("<h1>Hello, " + name + "!</h1>");
-        }
+        counter += 1;
+        out.println("<h1>The count is " + counter + ".</h1>");
     }
 }
+
