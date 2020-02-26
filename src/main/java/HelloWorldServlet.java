660c029d4525af283cf0f6aa86817e314523c05e
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-02-26 14:36:07 -0600

    fixing bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,20 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    private String name = "Dylan";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        if (name.equals(null) || name.equals("") || name.equals(" ")) {
-            out.println("<h1>Hello, World!</h1>");
-        } else {
-            out.println("<h1>Hello, " + name + "!</h1>");
+        if(name == null){
+            name = "World";
         }
+        response.getWriter().println("<h1>Hello, " + name + "!<h1>");
     }
 }
